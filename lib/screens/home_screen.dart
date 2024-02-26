06b01f927598f024846b72a553c28808176ab8c8
Lans12345 <olanalans12345@gmail.com>
2024-02-27 07:45:36 +0800

    added initial home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:matching_an_outfit/utlis/colors.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
+  void onTabTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  final List<Widget> children = [
+    for (int i = 0; i < 3; i++) const SizedBox(),
+  ];
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: background,
+      bottomNavigationBar: BottomNavigationBar(
+        selectedLabelStyle: const TextStyle(fontFamily: 'Bold'),
+        unselectedLabelStyle: const TextStyle(fontFamily: 'Bold'),
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: onTabTapped,
+        currentIndex: currentIndex,
+        backgroundColor: primary,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.panorama_vertical_select_outlined),
+            label: 'My Outfit',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Me',
+          ),
+        ],
+      ),
+    );
   }
 }
